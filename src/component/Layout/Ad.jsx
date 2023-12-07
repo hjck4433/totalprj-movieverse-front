84b5d3bf57abbb32d04abea61ce5b7a48a585a9f
pshyun123 <pshyun7204@gmail.com>
2023-12-07 20:05:31 +0900

    소현 광고 추가-> 결제페이지로 이동

---
+++ src/component/Layout/Ad.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import face from "../../images/faceIcon/faceIcon1.png";
+import { useNavigate } from "react-router-dom";
+const AdComp = styled.div`
+  width: 100%;
+  background-color: var(--VIOLET);
+
+  .container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+  }
+  &:hover {
+    cursor: pointer;
+    transition: 0.2s;
+    background-color: var(--LIGHTVIO);
+
+    p {
+      color: var(--DARKBLUE);
+    }
+  }
+  .ad {
+    width: 65%;
+    padding-left: 7%;
+    display: flex;
+    justify-content: center;
+    align-items: left;
+    flex-direction: column;
+    p {
+      font-size: 1.5rem;
+      padding-bottom: 10px;
+    }
+    @media only screen and (max-width: 768px) {
+      p {
+        font-size: 1.2rem;
+      }
+      .container {
+        width: 90vw;
+      }
+    }
+  }
+  .imgWrap {
+    width: 20%;
+    padding-bottom: 20%;
+    position: relative;
+    .faceIcon {
+      position: absolute;
+      /* width: 70%; */
+      height: 100%;
+      top: 0;
+      left: 0;
+    }
+  }
+`;
+const Advertise = ({ onNext }) => {
+  const navigate = useNavigate();
+  const [clicked, setClicked] = useState(false);
+
+  const containerClickHandler = () => {
+    setClicked(true);
+  };
+
+  useEffect(() => {
+    if (clicked) {
+      navigate("/Payment");
+    }
+  }, [clicked, navigate]);
+
+  return (
+    <>
+      <AdComp onClick={containerClickHandler}>
+        <div className="container">
+          <div className="ad">
+            <p>지금 멤버십에 가입하고 광고 없는</p>
+            <p>쾌적한 환경에서 무비버스를 이용해보세요!</p>
+          </div>
+          <div className="imgWrap">
+            <img className="faceIcon" src={face} alt="faceIcon" />
+          </div>
+        </div>
+      </AdComp>
+    </>
+  );
+};
+export default Advertise;
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../component/Layout/Header/Header";
 import Footer from "../component/Layout/Footer";
+import Advertise from "../component/Layout/Ad";
 
 const Layout = () => {
   return (
@@ -9,6 +10,7 @@ const Layout = () => {
       <main>
         <Outlet />
       </main>
+      <Advertise />
       <Footer />
     </>
   );
