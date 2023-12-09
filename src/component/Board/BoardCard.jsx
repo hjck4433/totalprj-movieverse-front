e6f3d16c30c4d0c2e794fe64378a4e84da420dd7
ljw <dlwodnjs2669@gmail.com>
2023-12-09 19:58:10 +0900

    재원 boardCard,  boardStyle, boardList 추가

--- src/component/Board/BoardCard.jsx
+++ src/component/Board/BoardCard.jsx
@@ -1,10 +1,24 @@
 const BoardCard = ({ board }) => {
   return (
     <>
-      <div>{board.image}</div>
-      <div>{board.gatherType}</div>
-      <div>{board.title}</div>
-      <div>{board.content}</div>
+      <div className="mapBox">
+        <div className="imgBox">
+          <img src={board.image} alt="이미지" />
+        </div>
+        <div className="textBox">
+          <div className="categoryGatherBox">
+            <div className="categoryGather">
+              <p className="gather">{board.categoryId}</p>
+              <p className="category">{board.gatherType}</p>
+            </div>
+            <div className="regdate">
+              <p className="regdate">{board.regdate}</p>
+            </div>
+          </div>
+          <h3>{board.title}</h3>
+          <p className="content">{board.content}</p>
+        </div>
+      </div>
     </>
   );
 };
--- src/component/Board/BoardCardList.jsx
+++ src/component/Board/BoardCardList.jsx
@@ -1,93 +1,96 @@
 import BoardCard from "./BoardCard";
-
+import BoardCardStyle from "../Board/BoardCardStyle";
 const BoardCardList = () => {
   const BoardData = [
     {
       title: "크리스마스 이브에 케빈과 함께 보낼 사람 모여라🎅🎄1",
       content:
         "혼자 하는 크리스마스는 이제 그만! 크리스마스 하면 케빈이죠~뭅뭅 친구들끼리 온라인으로 모여 수다나 떨며 ‘나홀로 집에’ 함께봐요ㅎㅎ 각자 집에서 좋아하는 음식을 준비해서 편하게 모여요 :)",
-      categoryId: "Attend",
+      categoryId: "무비모임",
       regdate: "2023.09.06",
       count: "10",
       gatherType: "온라인",
       image:
-        "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/1_comp_wallet.jpg?alt=media&token=25e63758-5c94-4ecc-8ecc-0ccdda651a01",
+        "https://images.unsplash.com/photo-1451772741724-d20990422508?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       title: "크리스마스 이브에 케빈과 함께 보낼 사람 모여라🎅🎄2",
       content:
         "혼자 하는 크리스마스는 이제 그만! 크리스마스 하면 케빈이죠~뭅뭅 친구들끼리 온라인으로 모여 수다나 떨며 ‘나홀로 집에’ 함께봐요ㅎㅎ 각자 집에서 좋아하는 음식을 준비해서 편하게 모여요 :)",
-      categoryId: "Attend",
+      categoryId: "무비모임",
       regdate: "2023.09.06",
       count: "10",
       gatherType: "오프라인",
       image:
-        "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/1_comp_wallet.jpg?alt=media&token=25e63758-5c94-4ecc-8ecc-0ccdda651a01",
+        "https://images.unsplash.com/photo-1451772741724-d20990422508?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       title: "크리스마스 이브에 케빈과 함께 보낼 사람 모여라🎅🎄3",
       content:
         "혼자 하는 크리스마스는 이제 그만! 크리스마스 하면 케빈이죠~뭅뭅 친구들끼리 온라인으로 모여 수다나 떨며 ‘나홀로 집에’ 함께봐요ㅎㅎ 각자 집에서 좋아하는 음식을 준비해서 편하게 모여요 :)",
-      categoryId: "Attend",
+      categoryId: "무비모임",
       regdate: "2023.09.06",
       count: "10",
       gatherType: "온라인",
       image:
-        "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/1_comp_wallet.jpg?alt=media&token=25e63758-5c94-4ecc-8ecc-0ccdda651a01",
+        "https://images.unsplash.com/photo-1451772741724-d20990422508?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       title: "크리스마스 이브에 케빈과 함께 보낼 사람 모여라🎅🎄4",
       content:
         "혼자 하는 크리스마스는 이제 그만! 크리스마스 하면 케빈이죠~뭅뭅 친구들끼리 온라인으로 모여 수다나 떨며 ‘나홀로 집에’ 함께봐요ㅎㅎ 각자 집에서 좋아하는 음식을 준비해서 편하게 모여요 :)",
-      categoryId: "Attend",
+      categoryId: "무비모임",
       regdate: "2023.09.06",
       count: "10",
       gatherType: "오프라인",
       image:
-        "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/1_comp_wallet.jpg?alt=media&token=25e63758-5c94-4ecc-8ecc-0ccdda651a01",
+        "https://images.unsplash.com/photo-1451772741724-d20990422508?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       title: "크리스마스 이브에 케빈과 함께 보낼 사람 모여라🎅🎄5",
       content:
         "혼자 하는 크리스마스는 이제 그만! 크리스마스 하면 케빈이죠~뭅뭅 친구들끼리 온라인으로 모여 수다나 떨며 ‘나홀로 집에’ 함께봐요ㅎㅎ 각자 집에서 좋아하는 음식을 준비해서 편하게 모여요 :)",
-      categoryId: "Attend",
+      categoryId: "무비모임",
       regdate: "2023.09.06",
       count: "10",
       gatherType: "온라인",
       image:
-        "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/1_comp_wallet.jpg?alt=media&token=25e63758-5c94-4ecc-8ecc-0ccdda651a01",
+        "https://images.unsplash.com/photo-1451772741724-d20990422508?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       title: "크리스마스 이브에 케빈과 함께 보낼 사람 모여라🎅🎄6",
       content:
         "혼자 하는 크리스마스는 이제 그만! 크리스마스 하면 케빈이죠~뭅뭅 친구들끼리 온라인으로 모여 수다나 떨며 ‘나홀로 집에’ 함께봐요ㅎㅎ 각자 집에서 좋아하는 음식을 준비해서 편하게 모여요 :)",
-      categoryId: "Attend",
+      categoryId: "무비모임",
       regdate: "2023.09.06",
       count: "10",
       gatherType: "오프라인",
       image:
-        "https://firebasestorage.googleapis.com/v0/b/repairrun-dae01.appspot.com/o/1_comp_wallet.jpg?alt=media&token=25e63758-5c94-4ecc-8ecc-0ccdda651a01",
+        "https://images.unsplash.com/photo-1451772741724-d20990422508?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
   ];
   return (
-    <div className="container">
-      <div className="boardCardBox">
-        <div className="gatherTypeList"></div>
-        <p>온라인</p>
-        <p>오프라인</p>
-        <div className="boardSort">
-          <p>최신순</p>
-          <p>과거순</p>
-          <p>조회 많은 순</p>
-        </div>
-        <div className="boardMap">
+    <BoardCardStyle>
+      <div className="container">
+        <div className="boardCardBox">
+          <div className="gatherTypeList">
+            <p>온라인</p>
+            <p>오프라인</p>
+          </div>
+          <div className="boardSort">
+            <p>최신순 |</p>
+            <p>과거순 |</p>
+            <p>조회 많은 순</p>
+          </div>
+          {/* <div className="boardMap"> */}
           {BoardData &&
             BoardData.map((board) => (
               <BoardCard key={board.title} board={board} />
             ))}
+          {/* </div> */}
         </div>
       </div>
-    </div>
+    </BoardCardStyle>
   );
 };
 
