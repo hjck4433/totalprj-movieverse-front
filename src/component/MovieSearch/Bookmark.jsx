becd180e3e697323ce1e0962fac2e18d4c149ec6
TommyLee <kokokorkr@naver.com>
2023-12-11 20:20:22 +0900

    세웅 bookmark컴포넌트 추가 231211

--- src/component/MovieSearch/Bookmark.jsx
+++ src/component/MovieSearch/Bookmark.jsx
@@ -1,11 +1,31 @@
+import React, { useState } from "react";
+import filled_heart from "../../images/Bookmark/filled_heart.png";
+import empty_heart from "../../images/Bookmark/empty_heart.png";
 import { styled } from "styled-components";
 
-const BookmarkComp = styled.div``;
+const BookmarkComp = styled.div`
+  .BookmarkHeart {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+`;
 
 const Bookmark = () => {
+  const [filled, setFilled] = useState(false);
+  const ChangeHeart = () => {
+    setFilled(!filled);
+  };
+
   return (
     <BookmarkComp>
-      <div className="BookmarkBox"></div>
+      <div className="BookmarkHeart" onClick={ChangeHeart}>
+        {filled ? (
+          <img src={filled_heart} alt="Filled Heart" />
+        ) : (
+          <img src={empty_heart} alt="Empty Heart" />
+        )}
+      </div>
     </BookmarkComp>
   );
 };
