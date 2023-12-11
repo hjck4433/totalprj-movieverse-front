e479feb238332ccb28bd8f18d9f2f6f35282cc48
pshyun123 <pshyun7204@gmail.com>
2023-12-11 09:23:26 +0900

    소현 mainpage 작업중

--- src/component/Main/BoxOffcie.jsx
+++ src/component/Main/BoxOffcie.jsx
@@ -1,8 +1,53 @@
 import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const BoxOfficeComp = styled.section``;
+import "swiper/css";
+import "swiper/css/effect-cards";
+// import { EffectCards } from "swiper/modules";
+
+const BoxOfficeComp = styled.section`
+  width: 100%;
+  padding: 5% 0;
+  .container {
+    width: 100%;
+    padding: 100px 0;
+
+    .boxOfficeBox {
+      border: 1px solid red;
+      text-align: center;
+      align-items: center;
+      padding: 20px;
+      .textBox {
+        h3 {
+          font-weight: 600;
+        }
+        hr {
+          width: 130px;
+          margin-bottom: 30px;
+        }
+      }
+      .cardSwiper {
+      }
+    }
+  }
+`;
 const BoxOffice = () => {
-  return <></>;
+  return (
+    <>
+      <BoxOfficeComp>
+        <div className="container">
+          <div className="boxOfficeBox">
+            <div className="textBox">
+              <h3>박스 오피스</h3>
+              <hr />
+            </div>
+
+            <div className="cardSwiper">{/* 슬라이더 들어갈 자리 */}</div>
+          </div>
+        </div>
+      </BoxOfficeComp>
+    </>
+  );
 };
 export default BoxOffice;
--- src/component/Main/OTT.jsx
+++ src/component/Main/OTT.jsx
@@ -1,8 +1,79 @@
 import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
+import tiving from "../../images/logo_tiving.svg";
+import wavve from "../../images/logo_wavve.png";
+import watcha from "../../images/logo_watcha.svg";
 
-const OTTComp = styled.section``;
+const OTTComp = styled.section`
+  width: 100%;
+  padding: 5% 0;
+  background-color: white;
+  .container {
+    width: 100%;
+    padding: 100px 0;
+    .ottRankBox {
+      border: 1px solid red;
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      align-items: center;
+      padding: 20px;
+      h3 {
+        font-weight: 600;
+        color: black;
+      }
+      hr {
+        width: 130px;
+        margin-bottom: 30px;
+      }
+      .ottBtnBox {
+        border: 1px solid blue;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 50%;
+        button {
+          width: 130px;
+          height: 60px;
+          cursor: pointer;
+          margin: 0 10px;
+
+          img {
+            /* height: 60px; */
+            height: 100%;
+            width: 100%;
+          }
+        }
+      }
+      .swiperBasic {
+      }
+    }
+  }
+`;
 const OTT = () => {
-  return <></>;
+  return (
+    <>
+      <OTTComp>
+        <div className="container">
+          <div className="ottRankBox">
+            <h3>OTT별 순위</h3>
+            <hr />
+            <div className="ottBtnBox">
+              <button className="tivingBtn">
+                <img src={tiving} alt="tiving" />
+              </button>
+              <button className="wavveBtn">
+                <img src={wavve} alt="wavve" />
+              </button>
+              <button className="watcha">
+                <img src={watcha} alt="watcha" />
+              </button>
+            </div>
+            <div className="swiperBasic">{/* 기본 슬라이더 들어감 */}</div>
+          </div>
+        </div>
+      </OTTComp>
+    </>
+  );
 };
 export default OTT;
