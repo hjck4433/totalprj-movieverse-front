1ea520d3b2da84aa73ce89c5cc0ec66086da4a4c
hjck4433 <hjck4433@gmail.com>
2023-12-12 20:14:30 +0900

    현지 accessToken재발급 추가

--- src/util/Common.jsx
+++ src/util/Common.jsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Common = {
   MV_DOMAIN: "http://localhost:8111",
   MV_SOCKET_URL: "ws://localhost:8111/ws/chat",
@@ -13,6 +15,40 @@ const Common = {
   },
   getRefreshToken: () => {
     return localStorage.getItme("refreshToken");
+  },
+  // 헤더
+  tokenHeader: () => {
+    const accessToken = Common.getAccessToken();
+    return {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+  },
+
+  handleUnathorized: async () => {
+    const accessToken = Common.getAccessToken();
+    const refreshToken = Common.getRefreshToken();
+    console.log("refreshToken : " + refreshToken);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+    try {
+      const res = await axios.post(
+        `${Common.MV_DOMAIN}/auth/refresh`,
+        refreshToken,
+        config
+      );
+      console.log(res.data);
+      Common.setAccessToken(res.data.accessToken);
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
   },
 };
 
