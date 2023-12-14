348e16ceab01e625d2a704967a0b8c59ad928568
pshyun123 <pshyun7204@gmail.com>
2023-12-14 17:40:07 +0900

    소현 post 페이지 댓글 map 적용

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,8 +1,21 @@
+import React from "react";
 import { PostComp } from "../component/Post/PostStyle";
+import { useNavigate } from "react-router-dom";
 import faceIcon from "../images/faceIcon/faceIcon4.png";
 import Button from "../util/Button";
 import postImg from "../images/main_top_mo.jpg";
 const Post = () => {
+  const navigate = useNavigate();
+  const toBoard = () => {
+    navigate("/board/:id");
+  };
+  const commentData = [
+    {
+      faceIcon: "../images/faceIcon/faceIcon4.png",
+      nickName: "햄햄",
+      commentText: "저 초대해주세요!",
+    },
+  ];
   return (
     <>
       <PostComp>
@@ -69,40 +82,47 @@ const Post = () => {
           <div className="commentArea">
             <h3>댓글</h3>
             <div className="commentList">
-              <div className="commentBox">
-                <div className="iconArea">
-                  <div className="imgBox">
-                    <img src={faceIcon} alt="memberIcon" />
+              {commentData.map((comment) => (
+                <div key={comment.nickName} className="commentBox">
+                  <div className="iconArea">
+                    <div className="imgBox">
+                      <img
+                        src={comment.memberIcon || faceIcon}
+                        alt="memberIcon"
+                      />
+                    </div>
+                  </div>
+                  <div className="textArea">
+                    <div className="comment">
+                      <p className="nickName">{comment.nickName}</p>
+                      <p className="commentText">{comment.commentText}</p>
+                    </div>
+                  </div>
+                  <div className="rightArea">
+                    <div className="writeDate">2023.12.14</div>
+                    <div className="editBtnBox">
+                      <Button
+                        className="deleteBtn"
+                        children="삭제"
+                        active={true}
+                        width="70px"
+                        height="30px"
+                        fontSize="14px"
+                        front="var(--BLUE)"
+                      />
+                      <Button
+                        className="editBtn"
+                        children="수정"
+                        active={true}
+                        width="70px"
+                        height="30px"
+                        fontSize="14px"
+                        front="var(--GREY)"
+                      />
+                    </div>
                   </div>
                 </div>
-                <div className="textArea">
-                  <p className="nickName">햄햄</p>
-                  <p className="commentText">저 초대해주세요!</p>
-                </div>
-                <div className="rightArea">
-                  <div className="writeDate">2023.12.14</div>
-                  <div className="editBtnBox">
-                    <Button
-                      className="deleteBtn"
-                      children="삭제"
-                      active={true}
-                      width="70px"
-                      height="30px"
-                      fontSize="14px"
-                      front="var(--BLUE)"
-                    />
-                    <Button
-                      className="editBtn"
-                      children="수정"
-                      active={true}
-                      width="70px"
-                      height="30px"
-                      fontSize="14px"
-                      front="var(--GREY)"
-                    />
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
             <div className="textInputBox">
               <textarea type="text" placeholder="댓글을 남겨주세요."></textarea>
@@ -123,6 +143,7 @@ const Post = () => {
               active={true}
               front="var(--VIOLET)"
               back="var(--BLUE)"
+              clickEvt={toBoard}
             />
           </div>
         </div>
