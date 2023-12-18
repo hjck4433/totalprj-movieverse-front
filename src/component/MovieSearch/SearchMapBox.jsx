90c62e06fbcffa9f8c9cf512aae540f2109c398c
hjck4433 <hjck4433@gmail.com>
2023-12-19 05:09:43 +0900

    현지 영화 무한스크롤 추가

--- src/component/MovieSearch/SearchMapBox.jsx
+++ src/component/MovieSearch/SearchMapBox.jsx
@@ -3,12 +3,12 @@ import MovieCard from "./MovieCard";
 import React, { useEffect, useState, useRef } from "react";
 import MovieApi from "../../api/MovieApi";
 
-const SearchMapBoxStyle = styled.div`
+const SearchMapBoxStyle = styled.section`
   .container {
+    padding-bottom: 150px;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     gap: 40px;
-    margin-bottom: 100px;
   }
   @media only screen and (max-width: 768px) {
     .container {
@@ -21,81 +21,61 @@ const SearchMapBox = () => {
   const [movieSearchData, setMovieSearchData] = useState([]);
   const [currentPage, setCurrentPage] = useState(0); // 무한 스크롤에는 필요하지 않음
   const [loading, setLoading] = useState(false);
+  const [lastPage, setLastPage] = useState(false);
 
-  const containerRef = useRef(null);
+  const end = useRef(null);
 
-  useEffect(() => {
-    const fetchMovieSearchData = async () => {
-      try {
-        setLoading(true);
-        const response = await MovieApi.moviePageList(currentPage, 8);
+  const fetchMovieSearchData = async () => {
+    try {
+      setLoading(true);
+      const response = await MovieApi.moviePageList(currentPage, 8);
+      if (response.data.length === 0) {
+        //값이 없으면 마지막 페이지
+        setLastPage(true);
+      } else {
         setMovieSearchData((prevData) => [...prevData, ...response.data]);
-        setLoading(false);
-      } catch (error) {
-        console.error("영화 데이터를 불러오는 중 에러 발생:", error);
-        setLoading(false);
+        setCurrentPage(currentPage + 1);
       }
-    };
-
-    fetchMovieSearchData();
-  }, [currentPage]);
+      setLoading(false);
+    } catch (error) {
+      console.error("영화 데이터를 불러오는 중 에러 발생:", error);
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const handleScroll = () => {
-      const container = containerRef.current;
+    fetchMovieSearchData();
+  }, []);
 
-      if (container) {
-        const isBottom =
-          container.scrollTop + container.clientHeight ===
-          container.scrollHeight;
+  useEffect(() => {
+    if (!loading && !lastPage) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            fetchMovieSearchData();
+          }
+        },
+        { threshold: 1 }
+      );
+      observer.observe(end.current);
 
-        if (isBottom && !loading) {
-          setCurrentPage((prevPage) => prevPage + 1);
-        }
-      }
-    };
-
-    const container = containerRef.current;
-
-    console.log("useRef : " + containerRef.current?.scrollTop);
-
-    if (container) {
-      container.addEventListener("scroll", handleScroll);
-
+      // Cleanup function
       return () => {
-        container.removeEventListener("scroll", handleScroll);
+        observer.disconnect();
       };
     }
-  }, [loading]);
-
-  // // 무한 스크롤
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const isBottom =
-  //       window.innerHeight + document.documentElement.scrollTop ===
-  //       document.documentElement.offsetHeight;
-
-  //     if (isBottom && !loading) {
-  //       setCurrentPage((prevPage) => prevPage + 1);
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, [loading]);
+  }, [loading, lastPage]);
 
   return (
     <>
       <SearchMapBoxStyle>
-        <div className="container" ref={containerRef}>
+        <div className="container">
           {movieSearchData &&
             movieSearchData.map((movie) => (
-              <MovieCard movie={movie} key={movie.id} />
+              <MovieCard movie={movie} key={movie.title} />
             ))}
         </div>
+        {!loading && <div ref={end}></div>}
       </SearchMapBoxStyle>
     </>
   );
