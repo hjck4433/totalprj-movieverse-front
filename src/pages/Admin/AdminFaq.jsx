a63f42652e9a660c596d20be90097844b2926e31
11yhj <uouo_@naver.com>
2023-12-18 09:32:04 +0900

    현주 AdminFaq 제작

--- src/pages/Admin/AdminFaq.jsx
+++ src/pages/Admin/AdminFaq.jsx
@@ -1,7 +1,134 @@
+import { styled } from "styled-components";
+import Button from "../../util/Button";
+
+const AdminFaqComp = styled.div`
+  padding-top: 8%;
+  .container {
+    min-width: 1200px;
+    background-color: var(--VIOLET);
+    padding: 40px 30px;
+    margin: 0;
+    margin-left: 50px;
+    h2 {
+      margin-bottom: 30px;
+    }
+    .tableBox {
+      width: 100%;
+      padding-bottom: 10px;
+      overflow-x: auto;
+      table {
+        width: 100%;
+        max-width: 100%;
+        white-space: nowrap;
+        overflow-x: scroll;
+        thead {
+          tr {
+            border-radius: 10px;
+
+            th {
+              color: #333;
+              font-weight: 600;
+              font-size: 1.2em;
+              background-color: var(--GREY);
+              padding: 20px;
+              &:first-child {
+                border-radius: 10px 0 0 10px;
+              }
+              &:last-child {
+                border-radius: 0 10px 10px 0;
+              }
+            }
+          }
+        }
+        tbody {
+          tr {
+            td {
+              padding: 10px;
+              Button {
+              }
+              &.center {
+                text-align: center;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 const AdminFaq = () => {
+  const faqData = [
+    {
+      title: "무비버스는 무엇인가요?",
+    },
+    {
+      title: "무비버스는 무엇인가요?",
+    },
+    {
+      title: "무비버스는 무엇인가요?",
+    },
+  ];
   return (
     <>
-      <div>관리자 FAq</div>
+      <AdminFaqComp>
+        <div className="container">
+          <h2>FAQ 관리</h2>
+          <div className="tableBox">
+            <table>
+              <thead className="">
+                <tr>
+                  <th>No.</th>
+                  <th>제목</th>
+                  <th>수정</th>
+                  <th>삭제</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* map으로 반복할 요소 */}
+                {faqData &&
+                  faqData.map((data, index) => (
+                    <tr key={data.title}>
+                      <td className="center">{index + 1}</td>
+                      <td>{data.title}</td>
+                      <td>
+                        <Button
+                          children={"수정"}
+                          back="var(--BLUE)"
+                          fontSize=".8em"
+                          width="80px"
+                          height="30px"
+                          active={true}
+                          clickEvt={() => {}}
+                        />
+                      </td>
+
+                      <td>
+                        <Button
+                          children={"삭제"}
+                          fontSize=".8em"
+                          width="80px"
+                          height="30px"
+                          active={true}
+                          front="var(--BLUE)"
+                          clickEvt={() => {}}
+                        />
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+            <Button
+              children={"추가"}
+              fontSize=".8em"
+              width="80px"
+              height="30px"
+              active={true}
+              clickEvt={() => {}}
+            />
+          </div>
+        </div>
+      </AdminFaqComp>
     </>
   );
 };
